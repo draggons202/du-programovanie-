cbad72fd2e1ac124c4463b5f43bb05168f19bc25
draggons202 <94683849+draggons202@users.noreply.github.com>
2022-09-14 18:03:46 +0200

    Add files via upload
    dobry den pan učitel, chcel by som sa opytat že či by nebolo možne si veci zopakovat lebo cez prazdiny som to skoro všetko zabudol, tam som napisal len to čo som nezabudol 
    (ale su tam komentare :) )

---
+++ domaca uloiha samuel kašiar python.py
@@ -0,0 +1,23 @@
+import tkinter
+canvas = tkinter.Canvas(width=800, height=200, background='white')
+canvas.pack()
+entry1 = tkinter.Entry()
+entry1.pack()
+
+#vykreslenie platna
+
+def x():
+#funkcia vykresli
+    subor = open('zastavba_na_ulici.txt')
+
+#otvorenie suboru
+
+
+
+
+button1 = tkinter.Button(text='ulica', command=x)
+button1.pack()
+
+#vykreslenie buttonu
+
+canvas.mainloop()
